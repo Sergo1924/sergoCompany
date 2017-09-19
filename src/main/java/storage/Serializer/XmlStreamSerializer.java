ba35cd123a8a8294b3b6941f8ef7c1bc3b755553
Sergo1924 <baranov.c@yandex.ru>
2017-09-19 21:47:03 +0400

    HW8: Xml serialization implementaion

---
+++ src/main/java/storage/Serializer/XmlStreamSerializer.java
@@ -0,0 +1,36 @@
+package storage.Serializer;
+
+import model.*;
+import util.XmlParser;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by sergo on 18.09.2017.
+ */
+public class XmlStreamSerializer implements StreamSerializer {
+    private XmlParser xmlParser;
+
+    public XmlStreamSerializer() {
+        xmlParser = new XmlParser(
+                Resume.class, Organization.class, Link.class,
+                OrganizationSection.class, TextSection.class, ListSection.class, Organization.Position.class);
+    }
+
+    @Override
+    public void doWrite(Resume r, OutputStream os) throws IOException {
+        try (Writer w = new OutputStreamWriter(os, StandardCharsets.UTF_8)) {
+            xmlParser.marshall(r, w);
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try (Reader r = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+            return xmlParser.unmarshall(r);
+        }
+    }
+}
